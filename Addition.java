04b01763ee13562cb388806fc9cfdba5c8c054de
Glorious-VV <glorious.vargheseveliyath@gmail.com>
2022-09-13 21:21:57 +0530

    Second Step

--- Addition.java
+++ Addition.java
@@ -5,11 +5,14 @@ class Main {
     System.out.println("Enter two numbers");
     int first = 10;
     int second = 20;
-    
+    int third=30;
+     int fourth=30;
     System.out.println(first + " " + second);
-
+    System.out.println(third+ " " + fourth);
     
     int sum = first + second;
     System.out.println("The sum is: " + sum);
+    int sum2 = third+ fourth;
+    System.out.println("The sum is: " + sum2);
   }
 }
